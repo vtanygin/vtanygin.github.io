73ca9208e2dc4840610c1795bf9c28171f2749db
vtanygin <63617394+vtanygin@users.noreply.github.com>
2020-04-13 23:04:44 -0400

    More images and some js tests

---
+++ test.js
@@ -0,0 +1,28 @@
+﻿//idea here is we want to grab all the images on the home page, and focus in on them when the user clicks
+
+//a note about how JS and DOM work: any JS that interacts with the DOM won't be able to see it until the page actually renders.
+//You can force it to wait using the window.onload wrapper, or using the below snippet in jQuery:
+
+//$(function() {
+    // do stuff when DOM is ready
+//})
+
+//Or just stick it at the end of the body tag. 
+
+//jQuery syntax: $('div') is identical to document.querySelectorAll('div'); it grabs everything inside 'div' tags
+//$('.container') selects all elements with class "container"
+//$('#name') selects all elements with id "name"
+
+//CSS manipulation in jQuery
+//$(el).css('border-width', '20px'); is like el.style.borderWidth = '20px'
+//It's nice in that you can manipulate *any* CSS property by calling css('property', 'new value')
+
+//Actual code follows. Note that since we put the script tag loading this .js file at the end of the body, we can access all DOM elements...
+//...and we have access to jQuery (since that's in the head). In general, .js files can call on information from other .js files loaded earlier in the HTML document.
+
+$("#cacti1").click(function () {
+    ;
+});
+
+//I'd personally prefer to create a variable storing that cacti1 ID element (var cactusimage), and create a function storing the alert call (cactusalert)...
+//...and then call cactusimage.cactusalert() at the end. Seems easier to keep track of, but that's just me...
