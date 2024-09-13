9d1001815aaaad3a16619a895f0a33988af053e2
vtanygin <63617394+vtanygin@users.noreply.github.com>
2024-09-13 01:11:21 -0400

    not selector test

--- tag_worker.js
+++ tag_worker.js
@@ -78,7 +78,8 @@ $(document).ready(function ()
 
         var x = $(".tags")
         //var y = $(".books")
-        var y = $("div[class~='books']") 
+        //var y = $("div[class*='books']") 
+        var y = $("div:not([class*='books'])") 
         y.fadeOut()
         //var z = y.toArray()
 
